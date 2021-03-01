4bbf42fd7e9fa44593d143312f87f5b7a24a0e91
Nkemjika Obi <nkemjikaobi@Administrators-MacBook-Pro.local>
2021-03-01 11:57:19 +0100

    Appropriate BG images for weather

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,13 +1,12 @@
 import React from 'react'
-import sad from '../images/sad.png'
 
 const NotFound = () => {
     return (
         <div className='container notfound grid grid-1'>
             <h1>Not Found</h1>
-            <p class='lead'>The page you are looking for does not exist...</p>
-            <a href='/' class='btn btn-primary text-center'>Home</a>
-            <img src={sad} alt='sad emoji'></img>
+            <p className='lead'>The page you are looking for does not exist...</p>
+            <a href='/' className='btn btn-primary text-center'>Home</a>
+            <img src='/images/sad.png' alt='sad emoji'></img>
         </div>
     )
 }
